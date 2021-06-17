323aff14fdabc4be1fec352d016e4a765b221b66
mjjomdev <mjjom.dev@gmail.com>
2021-06-18 00:27:05 +0900

    ADD: function (draft)

--- 04_exchange-rate-calculator/script.js
+++ 04_exchange-rate-calculator/script.js
@@ -1,6 +1,21 @@
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swapEl = document.getElementById('swap');
+
+// Fetch exchange rates and updates DOM
 function calculate() {
-  fetch('items.json')
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
 }
+
+// Event Listeners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+
 calculate();
