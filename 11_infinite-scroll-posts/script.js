327c5e8787a787f25f722162b1c5aeb5593fa366
mjjomdev <mjjom.dev@gmail.com>
2021-06-27 00:10:31 +0900

    FIX: typo

--- 11_infinite-scroll-posts/script.js
+++ 11_infinite-scroll-posts/script.js
@@ -5,7 +5,7 @@ const filter = document.getElementById('filter');
 let limit = 5;
 let page = 1;
 
-// fetch poasts from API
+// fetch posts from API
 async function getPosts() {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
